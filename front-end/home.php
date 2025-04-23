d7a912f3fa7f0e7631087d79fa6ea69c7848e920
DJ Delfinado <157677908+DanieJDelfinado@users.noreply.github.com>
2025-04-23 14:37:25 +0800

    l

--- front-end/home.php
+++ front-end/home.php
@@ -1,4 +1,16 @@
+<?php
+session_start();
 
+// Check if the user is logged in
+if (!isset($_SESSION['user_id'])) {
+    // Redirect to login page if not logged in
+    header('Location: ../index.php');
+    exit;
+}
+
+// Retrieve the username from the session
+$username = $_SESSION['username'];
+?>
  
 <!DOCTYPE html>
 <html lang="en">
@@ -8,13 +20,28 @@
     <title>Home</title>
     <link rel="stylesheet" href="../layout/all.css">
 </head>
-<body style="background-color: black;">
+<body style="background-color:  #353333;">
     
     
         <?php include 'navbar.php';?>
     
-        
 
 
+
+<div class="card" style="width: 400px; margin: 30px auto;">
+  <div class="card-body"  style="background-color:#5e5e5e;">
+    <h5 class="card-title">What's on your mind, <?php echo htmlspecialchars($username); ?>?</h5>
+    <form>
+      <textarea 
+        class="form-control" 
+        placeholder="<?php echo htmlspecialchars($username); ?>" 
+        rows="4"
+        style="color: black; background-color:#e2e2e2;"></textarea>
+      <button type="submit" class="btn btn-primary mt-3">Post</button>
+    </form>
+  </div>
+</div>
+
+      
 </body>
 </html>
--- front-end/navbar.php
+++ front-end/navbar.php
@@ -1,34 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Navbar Test</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: pink; padding: 12px;">
-  <div class="container-fluid" style="background-color: pink;">
+  <div class="container-fluid">
     <a class="navbar-brand" href="#">RAWR</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+          <a class="nav-link active" href="home.php">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
+
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+             data-bs-toggle="dropdown" aria-expanded="false">
+            Profile
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><a class="dropdown-item" href="#">Logout</a></li>
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
       </ul>
+
+      <!-- Move the search form OUTSIDE of the <ul> -->
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
     </div>
   </div>
 </nav>
 
-<link rel="stylesheet" href="../layout/navbar.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<!-- Bootstrap JS -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
